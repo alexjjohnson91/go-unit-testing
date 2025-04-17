a0ae5f8692d4198561bc887749e40405ee4b4bf9
Alex Johnson <alex.johnson@ramseysolutions.com>
2025-04-17 08:16:17 -0500

    [WIP]: Thu Apr 17 08:16:17 CDT 2025

---
+++ src/solution/Solution_test.go
@@ -0,0 +1,27 @@
+package solution
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		name string
+		a, b int
+		want int
+	}{
+		{"add positives", 2, 3, 5},
+		{"add negatives", -1, -1, -2},
+		{"add mix", -5, 5, 0},
+		{"zero", 0, 0, 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Solution{A: tt.a, B: tt.b}
+			got := s.Add()
+			if got != tt.want {
+				t.Errorf("Add() = %d; want %d", got, tt.want)
+			}
+		})
+	}
+}
+
