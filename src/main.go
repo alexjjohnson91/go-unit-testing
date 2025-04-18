2934ab45757ae6683b0e8bb5dd8f390d922e2fec
Alex Johnson <alex.johnson@ramseysolutions.com>
2025-04-18 12:13:36 -0500

    [WIP]: Fri Apr 18 12:13:36 CDT 2025

--- src/main.go
+++ src/main.go
@@ -8,5 +8,7 @@ import (
 func main() {
 	x := solution.Solution{A: 2, B: 3}
 
-	fmt.Println("solution: " + fmt.Sprint(x.Mul()))
+	fmt.Println("add: " + fmt.Sprint(x.Add()))
+	fmt.Println("mul: " + fmt.Sprint(x.Mul()))
+	fmt.Println("sub: " + fmt.Sprint(x.Sub()))
 }
