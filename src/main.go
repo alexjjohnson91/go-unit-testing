c75a79ee017136e5983e6e56abd423ace2892e2f
Alex Johnson <alex.johnson@ramseysolutions.com>
2025-04-18 12:24:59 -0500

    [WIP]: Fri Apr 18 12:24:59 CDT 2025

--- src/main.go
+++ src/main.go
@@ -11,4 +11,5 @@ func main() {
 	fmt.Println("add: " + fmt.Sprint(x.Add(3, 4)))
 	fmt.Println("mul: " + fmt.Sprint(x.Mul(3, 4)))
 	fmt.Println("sub: " + fmt.Sprint(x.Sub(3, 4)))
+	fmt.Println("div: " + fmt.Sprint(x.Div(4, 3.00)))
 }
--- src/solution/Solution.go
+++ src/solution/Solution.go
@@ -1,5 +1,7 @@
 package solution
 
+import "log"
+
 type Solution struct {
   A int
   B int
@@ -15,4 +17,11 @@ func (s Solution) Sub(a int, b int) int {
 
 func (s Solution) Mul(a int, b int) int {
   return (a * b)
+}
+
+func (s Solution) Div(a float32, b float32) float32 {
+  if b == 0 {
+    log.Fatal("ERROR: cannot divide by 0")
+  }
+  return (a / b)
 }
